8e5c4d0bfc329c163e1d5f6443290021ea4e976f
bubusaurio <charritos33@hotmail.com>
2024-06-16 19:15:50 -0600

    Vista empleado agregada

---
+++ app/src/main/java/com/example/sstech_kotlin/ui/empleado/ModificarEmpleado.kt
@@ -0,0 +1,21 @@
+package com.example.sstech_kotlin.ui.empleado
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.sstech_kotlin.R
+
+class ModificarEmpleado : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_modificar_empleado)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
